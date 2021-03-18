cb9d4cb2eba36582410b1c81171e2d06a3b2111b
manu <1333906+manurautela@users.noreply.github.com>
2021-03-18 19:55:24 +0530

    Add interfaces for sysmon etw publisher

--- osquery/events/windows/sysmon_etw.cpp
+++ osquery/events/windows/sysmon_etw.cpp
@@ -7,6 +7,53 @@
  * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
  */
 
-// This file contains the code for implmenting sysmon etw publisher
-#include "sysmon_etw.h"
+
+#include <osquery/config/config.h>
+#include <osquery/core/flags.h>
+#include <osquery/events/windows/sysmon_etw.h>
+#include <osquery/logger/logger.h>
+#include <osquery/registry/registry_factory.h>
+
+
+namespace osquery {
+
+    REGISTER(SysmonEtwEventPublisher,
+            "event_publisher",
+            "sysmon_etw");
+
+    const std::string kOsqueryEtwSessioName = "Microsoft-Windows-Sysmon";
+
+    void SysmonEtwEventPublisher::configure() {
+        tearDown();
+
+        /// TODO: Add the logic to setup user trace session via krabs library
+    }
+
+    bool WINAPI SysmonEtwEventPublisher::processEtwRecord(PEVENT_RECORD pEvent) {
+        /// TODO: this call back is set in user trace created in configure()
+        // Once configured receives the event on the session via sysmon provider
+        // This is the core function which delivers the event to the appropriate
+        // subscriber based on opcode (event type) to either process, image load,
+        // registry or dnsquery subscribor. Passing along the event data on fire()
+        return true;
+    }
+
+    Status SysmonEtwEventPublisher::run() {
+        // setup the callback function for EVENT_RECORD to processEtwRecord
+        return Status::success();
+    }
+
+
+    void SysmonEtwEventPublisher::tearDown() {
+        // Cleanup any subscriber specific context here
+    }
+
+    bool SysmonEtwEventPublisher::shouldFire(const SysmonEtwSubscriptionContextRef& sc,
+            const SysmonEtwEventContextRef& ec) const {
+        // In our case we always fire the event, provided there is atleast one subscribor
+        return true;
+    }
+
+} // namespace osquery
+
 
--- osquery/events/windows/sysmon_etw.h
+++ osquery/events/windows/sysmon_etw.h
@@ -9,8 +9,103 @@
 
 #pragma once
 
+#include <windows.h>
+#include <evntcons.h>
+#include <evntrace.h>
 #include <memory>
-#include <string>
-#include <vector>
+#include <tdh.h>
 
-#include <osquery/utils/status/status.h>
+#include <osquery/events/eventpublisher.h>
+
+namespace osquery {
+
+    /**
+     * @brief Subscription details for Sysmon ETW Traces
+     *
+     * This context is specific to the Sysmon ETW traces.
+     */
+    struct SysmonEtwSubscriptionContext : public SubscriptionContext {
+        // TODO: This will be based on subscriber type like process events. We
+        // may have a context mentioning what all filters subscriber wishes to
+        // set on the events that we receive from sysmon's etw trace session.
+        // Accordingly we can apply them on the trace session and hand over the
+        // events received via eventcallback back to subscribor.
+        // guid, trace_name, keywords may be.
+
+        private:
+            friend class SysmonEtwPublisher;
+    };
+
+    /**
+     * @brief Event details for WindowsEventLogEventPublisher events.
+     *
+     * It is the responsibility of the subscriber to understand the best
+     * way in which to parse the event data. The publisher will convert the
+     * Event Log record into a boost::property_tree, and return the tree to
+     * the subscriber for further parsing and row population.
+     */
+    struct SysmonEtwEventContext : public EventContext {
+        /// Event Metadata associated with the record
+        unsigned long pid;
+
+        unsigned short eventId;
+
+        unsigned char level;
+
+        unsigned char channel;
+
+        unsigned long long uptime;
+
+        unsigned long long timestamp;
+
+        /// Relevant event data
+        std::map<std::string, std::string> eventData;
+
+        /// GUID associated with the ETW trace provider
+        GUID etwProviderGuid;
+    };
+
+
+    using SysmonEtwEventContextRef  = std::shared_ptr<SysmonEtwEventContext>; using
+        SysmonEtwSubscriptionContextRef = std::shared_ptr<SysmonEtwSubscriptionContext>;
+
+    /**
+     * @brief A Windows Event Log Publisher
+     *
+     * This EventPublisher allows EventSubscriber's to subscribe to Sysmon
+     * Etw Logs. Note we create a single trace session for Sysmon and
+     * funnel all the events received back to various subscribors.
+     * Within the publisher run loop, we decide based on event opcode
+     * which subscribor to call to. Then publisher passes on that event
+     * data back to subscribor accordingly.
+     */
+    class SysmonEtwEventPublisher
+        : public EventPublisher<SysmonEtwSubscriptionContext, SysmonEtwEventContext> {
+
+            // DECLARE_PUBLISHER("sysmon_etw");
+
+            public:
+            ///
+            bool shouldFire(const SysmonEtwSubscriptionContextRef& sc,
+                    const SysmonEtwEventContextRef& ec) const override;
+
+            void configure() override;
+
+            void tearDown() override;
+
+            /// The calling for beginning the thread's run loop.
+            Status run() override;
+
+            static bool WINAPI processEtwRecord(PEVENT_RECORD pEvent);
+
+            private:
+            /// we don't really have multiple traces(session)
+            std::vector<GUID> providerGuids_;
+
+            /// Map of trace name, to the GUID/Handle pair for ease of access
+            std::map<std::string, std::pair<GUID, TRACEHANDLE>> etw_handles_;
+
+            public:
+            // friend class SysmonEtwTests;
+        };
+} // namespace osquery
